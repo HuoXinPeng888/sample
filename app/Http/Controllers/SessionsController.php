e67aa48f105fe75dcefba552df94759287985ab8
max <939686734@qq.com>
2018-06-06 09:32:29 +0000

    登录 注册 记住我

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+
+class SessionsController extends Controller
+{
+    public function create()
+    {
+        return view('sessions.create');
+    }
+//        用户登录
+    public function store(Request $request)
+    {
+        $res = $this->validate($request,[
+            'email' => 'required|email',
+            'password'=>'required'
+        ]);
+
+        if (Auth::attempt($res,$request->has('remember'))){
+            session()->flash('success','欢迎回来');
+            return redirect()->route('users.show',[Auth::user()]);
+        }else{
+            session()->flash('danger','邮箱或密码输入错误');
+            return redirect()->back();
+        }
+    }
+//        用户推出
+    public function destroy()
+    {
+            Auth::logout();
+            session()->flash('success','成功退出');
+            return redirect('login');
+    }
+}
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -25,6 +25,8 @@ class UsersController extends Controller
             'email' => $request->email,
             'password' => bcrypt($request->password),
         ]);
+//        注册后自动登录
+        Auth::login($user);
         session()->flash('success','注册成功');
         return redirect()->route('users.show',[$user]);
     }
